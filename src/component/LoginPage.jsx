772a691de44b1a32aa6c74855b7d0ce88f79e1ea
Agustinnisa <nisaagustin6@gmail.com>
2024-10-04 20:12:47 +0700

    add loginpage and styling

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -1,25 +1,31 @@
 import React from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Row, Col } from "react-bootstrap";
 
 const LoginPage = ({ handleLogin }) => {
   return (
-    <Container>
-      <h2>Login</h2>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+    <Container className="login-container">
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <h2 className="text-center my-4">Login</h2>
+          <Form>
+            <Form.Group controlId="formBasicEmail" className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+            <Form.Group controlId="formBasicPassword" className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
 
-        <Button variant="primary" type="button" onClick={handleLogin}>
-          Login
-        </Button>
-      </Form>
+            <div className="d-grid">
+              <Button variant="primary" type="button" onClick={handleLogin}>
+                Login
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 };
