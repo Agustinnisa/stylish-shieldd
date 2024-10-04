a4505e83bd074400af28fce51cf3971c009517e6
Agustinnisa <nisaagustin6@gmail.com>
2024-10-04 09:40:06 +0700

    edit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import WeatherDashboard from './component/WeatherDashboard';
-import WeatherDetails from './component/WeatherDetails';
-import WeeklyForecast from './component/WeeklyForecast';
-import OutfitRecommendation from './component/OutfitRecommendation';
-import Footer from './component/Footer';
-import { Container, Navbar, Nav, Image } from 'react-bootstrap';
-import logo from './assets/barterstyle-logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import WeatherDashboard from "./component/WeatherDashboard";
+import WeatherDetails from "./component/WeatherDetails";
+import WeeklyForecast from "./component/WeeklyForecast";
+import OutfitRecommendation from "./component/OutfitRecommendation";
+import Footer from "./component/Footer";
+import { Container, Navbar, Nav, Image } from "react-bootstrap";
+import logo from "./assets/barterstyle-logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-
   return (
     <Router>
       <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
         <Container>
           <Navbar.Brand as={Link} to="/">
-            <Image src={logo} width="40" height="40" alt="Stylish Shield Logo" /> Stylish Shield
+            <Image
+              src={logo}
+              width="40"
+              height="40"
+              alt="Stylish Shield Logo"
+            />{" "}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
-              <Nav.Link as={Link} to="/weekly-forecast">Weekly Forecast</Nav.Link>
-              <Nav.Link as={Link} to="/outfit-recommendation">Outfit Recommendation</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Dashboard Cuaca
+              </Nav.Link>
+              <Nav.Link as={Link} to="/details">
+                Detail Cuaca
+              </Nav.Link>
+              <Nav.Link as={Link} to="/weekly-forecast">
+                Prakiraan Mingguan
+              </Nav.Link>
+              <Nav.Link as={Link} to="/outfit-recommendation">
+                Rekomendasi Outfit
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -36,17 +47,16 @@ function App() {
           <Route path="/" element={<WeatherDashboard />} />
           <Route path="/details" element={<WeatherDetails />} />
           <Route path="/weekly-forecast" element={<WeeklyForecast />} />
-          <Route path="/outfit-recommendation" element={<OutfitRecommendation />} />
+          <Route
+            path="/outfit-recommendation"
+            element={<OutfitRecommendation />}
+          />
         </Routes>
       </Container>
 
-      <Footer className="bg-dark text-white text-center py-3 mt-4">
-        <Container>
-          <p>© 2024 Stylish Shield. All rights reserved.</p>
-        </Container>
-      </Footer>
+      <Footer />
     </Router>
   );
-};
+}
 
-export default App
+export default App;
